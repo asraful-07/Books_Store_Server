4926e0aa093a6fbfcd9876f73810417c957374c5
asraful-07 <asraful0508@gmail.com>
2025-07-26 19:14:52 +0600

    add book stroe router

--- pkg/routes/book_store_routes.go
+++ pkg/routes/book_store_routes.go
@@ -8,7 +8,7 @@ import (
 
 func RegisterBookStorRoutes(router *mux.Router) {
 	router.HandleFunc("/book", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookID}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookID}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/{bookID}", controllers.DeleteBook).Methods("DELETE")
