220a740d5ef4c4233627e7bd77f6dfc8c3727817
asraful-07 <asraful0508@gmail.com>
2025-08-06 18:20:32 +0600

    add book struct

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -22,10 +22,6 @@ type Book struct {
 	Publication  string             `json:"publication,omitempty" bson:"publication,omitempty"`
 	ProductType  string             `json:"productType,omitempty" bson:"productType,omitempty"`
 	PublishDate  string             `json:"publishDate,omitempty" bson:"publishDate,omitempty"`
-	// Color        []string           `json:"color,omitempty" bson:"color,omitempty"` 
-	// Size         string             `json:"size,omitempty" bson:"size,omitempty"`  
-	// Material     []string           `json:"material,omitempty" bson:"material,omitempty"`
-	// Quantity     int                `json:"quantity,omitempty" bson:"quantity,omitempty"`
 	Price        float64            `json:"price,omitempty" bson:"price,omitempty"`
 	OldPrice     float64            `json:"oldPrice,omitempty" bson:"oldPrice,omitempty"`
 	Stock        int                `json:"stock,omitempty" bson:"stock,omitempty"` 
